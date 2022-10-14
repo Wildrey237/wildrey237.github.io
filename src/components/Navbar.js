d69d9ace1335d42db6d9ad3cf5520b71aff0a1a6
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-10-15 01:18:53 +0200

    tout au propre

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+
+
+function Navbar() {
+    return (
+        <div className>
+            <ul>
+                <li className="Navbar_element">About</li>
+                <li className="Navbar_element">MySkills</li>
+                <li className="Navbar_element">Work</li>
+                <li className="Navbar_element">Contact</li>
+                <li className="Navbar_element">Blog</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
