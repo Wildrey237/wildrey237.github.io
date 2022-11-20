f5d239006e2f6f62a702483c66f3994fee951871
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-11-20 21:34:44 +0100

    work

--- src/home.js
+++ src/home.js
@@ -3,18 +3,22 @@ import Navbar from "./components/Navbar";
 
 function Home() {
     const profile_photo = "https://cdn.discordapp.com/attachments/958463954206732298/1038050944971190282/Wilfried_NB.png"
+    const cv ="https://cdn.discordapp.com/attachments/958463954206732298/1043907012834099260/CV_ECOLE.pdf"
   return (
     <main className="page">
-      <section className="main">
-          <article>
-              <h1>Salut, je suis Wilfried, Etudiant en informatique</h1>
+      <section className="main-home">
+          <article className={"article-text"}>
+              <h1 className={"h1-home"}>Salut, je suis Wilfried, Etudiant en informatique</h1>
+              <h2 className={"h2-home"}> A la recherche d'un stage de 4 mois dans la data et l'intelligence artificielle</h2>
               <p className="type_machine"> En Bachelor 2e année à ECE Paris</p>
               <br /><br />
-              <a className="green_empty_btn" href="/contact">Contactez-moi</a>
+              <div className={"btn-Contact"}>
+                  <a className="green_empty_btn" href="/contact">Contactez-moi</a> <a className="green_empty_btn" href={cv}>Uploader mon CV</a>
+              </div>
           </article>
-        </section>
-        <section>
-            <img className={"img-profile"} src={profile_photo}/>
+          <article className={"article-photo"}>
+              <img className={"img-profile"} src={profile_photo}/>
+          </article>
         </section>
       <div className="navbar_container">
         <Navbar/>
