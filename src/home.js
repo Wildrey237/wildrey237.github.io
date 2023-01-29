58493bee767e93f699f12b795c5a88050c5246d5
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-01-29 16:43:31 +0100

    Navbar.js responsive new style

--- src/home.js
+++ src/home.js
@@ -1,31 +1,46 @@
 import Navbar from "./components/Navbar";
+import {useState} from "react";
 
 
 function Home() {
     const profile_photo = "https://cdn.discordapp.com/attachments/958463954206732298/1038050944971190282/Wilfried_NB.png"
     const cv ="https://cdn.discordapp.com/attachments/958463954206732298/1043907012834099260/CV_ECOLE.pdf"
+    const [navbar, setNavbar] = useState(true);
+    const [navbarMobile, setNavbarMobile] = useState(true);
   return (
-    <main className="page">
+    <main className="page-home">
       <section className="main-home">
-          <article className={"article-text"}>
-              <h1 className={"h1-home"}>Salut, je suis Wilfried, Etudiant en informatique</h1>
+          <article className={"article-home"}>
+              <h1 className={"h1-home"}>Salut, je suis Wilfried Etudiant en informatique</h1>
               <h2 className={"h2-home"}> A la recherche d'un stage de 4 mois dans la data et l'intelligence artificielle</h2>
-              <p className="type_machine"> En Bachelor 2e année à ECE Paris</p>
+              <p className={"type_machine"}> En Bachelor 2e année à ECE Paris</p>
               <br /><br />
               <div className={"btn-Contact"}>
-                  <a className="green_empty_btn" href="/contact">Contactez-moi</a> <a className="green_empty_btn" href={cv}>Uploader mon CV</a>
+                  <a href={"/contact"}><input type={"button"} className={"green_empty_btn"} value={"Contactez-moi"}/></a> <a href={cv}> <input type={"button"} className={"green_empty_btn"} value={"Uploader mon CV"}/> </a>
               </div>
           </article>
           <article className={"article-photo"}>
               <img className={"img-profile"} src={profile_photo}/>
           </article>
         </section>
-      <div className="navbar_container">
+        <div className={"navbar_container"}>
         <Navbar/>
       </div>
-      <div className="navbar_mobile_container">
-        <Navbar/>
-      </div>
+        <div className={"navbar-bottom"}>
+            {navbar ? (
+                    <div onClick={()=>{
+                        setNavbar(false);
+                    }
+                    }>Navbar</div>
+                    ) : (
+                        <div className={"navbar_mobile_container"}>
+                            <div onClick={()=>{
+                                setNavbar(true);
+                            }
+                            }>Close</div>
+                            <Navbar/>
+                        </div>) }
+        </div>
     </main>
   );
 }
--- src/myskill.js
+++ src/myskill.js
@@ -1,51 +1,68 @@
 import Navbar from "./components/Navbar";
 
-
+let logoPHP ="https://www.freepnglogos.com/uploads/php-logo-png/php-logo-php-elephant-logo-vectors-download-5.png";
+let logoJava ="";
+let logoJS ="https://www.freepnglogos.com/pics/javascript";
+let logoPython ="";
 function Myskill() {
   return (
     <main className="page">
 
       <section className="main-skills">
-          <article>
+          <article className={"skill-block"}>
               <h1>Programation</h1>
-              <p className="type_machine">Php,</p>
-              <h2> Framework </h2>
-              <p>Symfony</p><br></br>
-              <p className="type_machine">Python </p>
-              <h2> Framework </h2>
-              <p>DJANGO</p><br></br>
-              <p> Python pour la creation d'application WEB</p><br></br>
-              <p className="type_machine">Java</p><br></br>
-              <p className="type_machine">Javascript</p><br></br>
+              <div className={"text-logo"}>
+                  <div data-width="95%" className={"hello"}></div>
+                  <div><p>Php</p></div>
+                  <div><img src={logoPHP} className={"skill-logo"}></img></div>
+              </div>
+              <div className="modal-content">
+                  <h2>Framework</h2>
+                  <p>Symfony</p><br></br>
+              </div>
+              <p className={""}>Python </p>
+              <div className="modal-content">
+                  <h2> Framework </h2>
+                  <p>DJANGO</p><br></br>
+                  <p>Python pour la creation d'application WEB</p><br></br>
+                  <p>Anaconda</p>
+                  <p>Python pour l'analyse de données</p><br></br>
+              </div>
+              <p className={""}>Java</p><br></br>
+              <p className={""}>Javascript - NodeJs</p><br></br>
               <h2> Framework </h2>
               <p>React Js</p><br></br>
               <br/><br/>
           </article>
 
-          <article>
+          <article className={'skill-block'}>
               <h1>Base de donnée</h1>
-              <p className="type_machine">MERISE</p><br></br>
-              <p className="type_machine">SQL</p>   <br></br>
-              <p className="type_machine">FireBase</p><br></br>
-              <p className="type_machine"> System Inter Cache</p><br></br>
+              <p className={""}>SQL</p>   <br></br>
+              <p className={""}>FireBase</p><br></br>
+              <p className={""}>System Inter Cache</p><br></br>
+              <p className={""}>MERISE</p><br></br>
               <br /><br />
           </article>
 
-          <article>
+          <article className={'skill-block'}>
               <h1>Marketing</h1>
-              <p className="type_machine">Marketing digital,</p><br></br>
-              <p className="type_machine">Gestion et analyse des données digitales ,</p><br></br>
-              <p className="type_machine">SEO,</p><br></br>
-              <p className="type_machine">Google Analytics,</p><br></br>
+              <p className={""}>Marketing digital</p><br></br>
+              <p className={""}>Gestion et analyse des données digitales</p><br></br>
+              <p className={""}>SEO</p><br></br>
+              <p className={""}>Google Analytics</p><br></br>
               <br /><br />
           </article>
 
-          <article>
+          <article className={'skill-block'}>
               <h1>Environement-Reseau</h1>
-              <p className="type_machine">Linux</p>
-              <p> Creation d'un serveur Web Linux et communication en SSH</p>
-              <p className="type_machine">Cloud computing</p>
-              <p> Creation d'un serveur Web avec Google Cloud et node.js</p>
+              <p className={""}>Linux - Ubuntu</p>
+              <div>
+                  <p> Creation d'un serveur Web Linux et communication en SSH</p>
+              </div>
+              <p className={""}>Cloud computing</p>
+              <div>
+                  <p>Creation d'un serveur Web avec Google Cloud et node.js</p>
+              </div>
               <br></br>
           </article>
 
