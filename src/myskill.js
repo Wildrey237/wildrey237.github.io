2ee32afecc62ef89504b0fee0069d04d58fdb747
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-06-13 07:40:12 +0200

    change

--- src/myskill.js
+++ src/myskill.js
@@ -10,64 +10,56 @@ function Myskill() {
 
       <section className="main-skills">
           <article className={"skill-block"}>
-              <h1>Programation</h1>
-              <div className={"text-logo"}>
-                  <div data-width="95%" className={"hello"}></div>
-                  <div><p>Php</p></div>
-                  <div><img src={logoPHP} className={"skill-logo"}></img></div>
-              </div>
+              <h1 className={"h1-skill"}>Programation</h1>
+              <h2 className={"h2-skill"}>PHP</h2>
+              <h2 className={"h2-skill"}>Python </h2>
               <div className="modal-content">
-                  <h2>Framework</h2>
-                  <p>Symfony</p><br></br>
-              </div>
-              <p className={""}>Python </p>
-              <div className="modal-content">
-                  <h2> Framework </h2>
+                  <span> Framework </span>
                   <p>DJANGO</p><br></br>
                   <p>Python pour la creation d'application WEB</p><br></br>
                   <p>Anaconda</p>
                   <p>Python pour l'analyse de données</p><br></br>
               </div>
-              <p className={""}>Java</p><br></br>
-              <p className={""}>Javascript - NodeJs</p><br></br>
+              <p className={"h2-skill"}>Java</p><br></br>
+              <p className={"h2-skill"}>Javascript - NodeJs</p><br></br>
               <h2> Framework </h2>
               <p>React Js</p><br></br>
               <br/><br/>
           </article>
 
           <article className={'skill-block'}>
-              <h1>Base de donnée</h1>
-              <p className={""}>SQL</p>   <br></br>
-              <p className={""}>FireBase</p><br></br>
-              <p className={""}>System Inter Cache</p><br></br>
-              <p className={""}>MERISE</p><br></br>
+              <h1 className={"h1-skill"}>Base de donnée</h1>
+              <h2 className={"h2-skill"}>SQL</h2>   <br></br>
+              <h2 className={"h2-skill"}>FireBase</h2><br></br>
+              <h2 className={"h2-skill"}>System Inter Cache</h2><br></br>
+              <h2 className={"h2-skill"}>MERISE</h2><br></br>
               <br /><br />
           </article>
 
           <article className={'skill-block'}>
-              <h1>Marketing</h1>
-              <p className={""}>Marketing digital</p><br></br>
-              <p className={""}>Gestion et analyse des données digitales</p><br></br>
-              <p className={""}>SEO</p><br></br>
-              <p className={""}>Google Analytics</p><br></br>
-              <br /><br />
-          </article>
-
-          <article className={'skill-block'}>
-              <h1>Environement-Reseau</h1>
-              <p className={""}>Linux - Ubuntu</p>
+              <h1 className={"h1-skill"}>Environement-Reseau</h1>
+              <h2 className={"h2-skill"}>Linux - Ubuntu</h2>
               <div>
-                  <p> Creation d'un serveur Web Linux et communication en SSH</p>
+                  <p> Creation de serveur Web Linux et communication en SSH</p>
               </div>
-              <p className={""}>Cloud computing</p>
+              <h2 className={"h2-skill"}>Cloud computing</h2>
               <div>
-                  <p>Creation d'un serveur Web avec Google Cloud et node.js</p>
+                  <p>Creation de serveur Web avec Google Cloud et node.js</p>
               </div>
               <br></br>
           </article>
 
+          {/*<article className={'skill-block'}>*/}
+          {/*    <h1>Marketing</h1>*/}
+          {/*    <h2 className={""}>Marketing digital</h2><br></br>*/}
+          {/*    <h2 className={""}>Gestion et analyse des données digitales</h2><br></br>*/}
+          {/*    <h2 className={""}>SEO</h2><br></br>*/}
+          {/*    <h2 className={""}>Google Analytics</h2><br></br>*/}
+          {/*    <br /><br />*/}
+          {/*</article>*/}
+
         <br /><br />
-        </section>
+      </section>
       <div className="navbar_container">
         <Navbar/>
       </div>
